267675ae7182fe527eb2b067ca869d3aacf53cbd
tarymee <tarymee@gmail.com>
2018-12-27 19:01:02 +0800

    up

--- tarymee-server/s.js
+++ tarymee-server/s.js
@@ -23,38 +23,62 @@ console.log(mime.getExtension('text/plain'))
 // https://www.cnblogs.com/SheilaSun/p/7271883.html
 
 const server = http.createServer(function (request, response) {
-    // request 是一个 http.IncomingMessage 实例，它是可读流。
-    // response 是一个 http.ServerResponse 实例，它是可写流。
+    // response.setHeader('Cache-Control', 'max-age=43200')
+    // response.writeHead(200, {
+    //     'Content-Type': 'text/plain',
+    //     'Cache-Control': 'max-age=43200'
+    // })
+    // response.write(JSON.stringify(request.headers))
+    // response.write('<br />')
+    // return response.end('结束')
 
-    // console.log(request.headers)
-    // for (var x in request) {
-    //     console.log(x)
-    // }
-    console.log(request.method)
-    console.log(url.parse(request.url, true))
-    request.on('data', (chunk) => {
 
-    })
-    request.on('end', () => {
-        // response.setHeader('Cache-Control', 'max-age=43200')
-        // console.log(response.headers)
-        // for (var x in response) {
-        //     console.log(x)
-        // }
-        response.writeHead(200, {
-            'Content-Type': 'text/plain',
-            'Cache-Control': 'max-age=43200'
-        })
-        console.log(response.getHeaders())
 
-        response.write(JSON.stringify(request.headers))
-        response.write('<br />')
-        return response.end('结束')
-    })
 
 
 })
 
-server.listen(8888)
+server.listen(config.port)
 
-console.log('server listen on port %d', port)
+console.log('server listen on port %d', config.port)
+
+
+
+
+// var http = require("http");
+// var url = require("url");
+// var fs = require("fs");
+// var server = http.createServer(function (req, res) {
+//     var req_path = url.parse(req.url).path;
+//     var filepath = __dirname + req_path;
+
+//     fs.exists(filepath, function (exists) {
+//         if (exists) {
+//             fs.stat(filepath, function (err, stats) {
+//                 if (err) {
+//                     res.writeHead(500, { 'Content-Type': 'text/html;charset=utf8' });
+//                     res.end('<div styel="color:black;font-size:22px;">server error</div>');
+//                 } else {
+//                     if (stats.isFile()) {
+//                         var file = fs.createReadStream(filepath);
+//                         res.writeHead(200, { 'Content-Type': 'text/html;charset=utf8' });
+//                         file.pipe(res);
+//                     } else {
+//                         fs.readdir(filepath, function (err, files) {
+//                             var str = '';
+//                             for (var i in files) {
+//                                 str += files[i] + '<br/>';
+//                             }
+//                             res.writeHead(200, { 'Content-Type': 'text/html;charset=utf8' });
+//                             res.write(str);
+//                         });
+//                     }
+//                 }
+//             });
+//         } else {
+//             res.writeHead(404, { 'Content-Type': 'text/html;charset=utf8' });
+//             res.end('<div styel="color:black;font-size:22px;">404 not found</div>');
+//         }
+//     });
+// });
+// server.listen('9090', '127.0.0.1');
